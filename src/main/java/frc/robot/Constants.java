8662a47142d0c2ac8baacc814c104eff4be2a66b
titanium-47 <rsidharth53@gmail.com>
2022-05-19 18:22:32 -0700

    finished swerve drive teleop

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,12 +4,35 @@
 
 package frc.robot;
 
+import com.swervedrivespecialties.swervelib.Mk4SwerveModuleHelper;
+import frc.robot.utils.SwerveModuleConfig;
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public final static class kSwerveDrive {
+        public static final Mk4SwerveModuleHelper.GearRatio GEAR_RATIO = Mk4SwerveModuleHelper.GearRatio.L1;
+        public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0;
+        public static final double DRIVETRAIN_WHEELBASE_METERS = 0;
+        public static final double MAX_VELOCITY_METERS_PER_SECOND = 0;
+        public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = 0;
+        public static final double MAX_VOLTAGE = 0;
+
+        public static final SwerveModuleConfig FRONT_LEFT = new SwerveModuleConfig(-1,-1,-1, 0.0);
+        public static final SwerveModuleConfig FRONT_RIGHT = new SwerveModuleConfig(-1, -1, -1, 0.0);
+        public static final SwerveModuleConfig BACK_LEFT = new SwerveModuleConfig(-1, -1, -1, 0.0);
+        public static final SwerveModuleConfig BACK_RIGHT = new SwerveModuleConfig(-1, -1, -1, 0.0);
+    }
+    public final static class kOI {
+        public static final int DRIVE_CONTROLLER = 1;
+    }
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.subsystems.SwerveDrive;
+import frc.robot.OI;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,9 +19,8 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final SwerveDrive swerveDrive = new SwerveDrive();
+  private final XboxController driveController = new XboxController(Constants.kOI.DRIVE_CONTROLLER); 
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -36,6 +36,10 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {}
 
+  public void teleopDrive() {
+    swerveDrive.setDefaultCommand(new RunCommand(() -> swerveDrive.drive(OI.getLeftStickX(driveController),
+    OI.getLeftStickY(driveController), OI.getRightStickX(driveController)), swerveDrive));
+  }
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
@@ -43,6 +47,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
   }
 }
